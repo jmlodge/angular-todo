3edd6f772865348e831f5e436942960ea5d7b54e
jmlodge <jm-lodge@hotmail.co.uk>
2016-09-07 18:56:20 +0100

    trouble with two-way binding and not displaying alert messages

--- js/controller.js
+++ js/controller.js
@@ -2,16 +2,23 @@ angular.module('RouteControllers', [])
     .controller('HomeController', function($scope) {
         $scope.title = "Welcome To Angular Todo!";
     })
-    .controller("RegisterController", function($scope) {
-
-    	$scope.registrationUser = {};
-
-    	$scope.submitForm = function() {
-	    	if ($scope.registrationForm.$valid) {
-	    		$scope.registrationUser.username = $scope.user.username;
-	    		$scope.registrationUser.password = $scope.user.password;
-	    	}
-
-	    	console.log($scope.registrationUser.username + " " + $scope.registrationUser.password);
-    	}
+    .controller('RegisterController', function($scope, $location, UserAPIService, store) {
+ 
+        $scope.registrationUser = {};
+        var URL = "https://morning-castle-91468.herokuapp.com/";
+ 
+        $scope.submitForm = function() {
+            if ($scope.registrationForm.$valid) {
+                $scope.registrationUser.username = $scope.user.username;
+                $scope.registrationUser.password = $scope.user.password;
+ 
+                UserAPIService.callAPI(URL + "accounts/register/", $scope.registrationUser).then(function(results) {
+                    $scope.data = results.data;
+                    alert("You have successfully registered to Angular Todo");
+                }).catch(function(err) {
+                    alert("Oops! Something went wrong!");
+                    console.log(err);
+                });
+            }
+        }
     });
